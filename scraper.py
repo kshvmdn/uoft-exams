497363f8b6f8eea1530ed1032becf445ae0521ab
Kashav Madan <kshvmdn@gmail.com>
2016-02-28 06:20:09 -0500

    Check if schedule is avail. for given year

--- scraper.py
+++ scraper.py
@@ -13,15 +13,16 @@ def request(url):
 def parse(resp):
     courses = []
     soup = BeautifulSoup(resp.text, 'html.parser')
-    rows = soup.find('div', id='content').find('table').find_all('tr')
-    headers = [col.text.lower() for col in rows[0].find_all('th')]
-    for row in rows[1:]:
-        td = [col.text for col in row.find_all('td')]
-        course = {}
-        for i in range(len(headers)):
-            course[headers[i]] = td[i]
-        courses.append(course)
-    return courses
+    if soup.find('div', id='content').find('table'):
+        rows = soup.find('div', id='content').find('table').find_all('tr')
+        headers = [col.text.lower() for col in rows[0].find_all('th')]
+        for row in rows[1:]:
+            td = [col.text for col in row.find_all('td')]
+            course = {}
+            for i in range(len(headers)):
+                course[headers[i]] = td[i]
+                courses.append(course)
+        return courses
 
 
 if __name__ == '__main__':
