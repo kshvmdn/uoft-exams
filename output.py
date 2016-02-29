cc0ed333be898a132510e1be4a1153d72883668a
Kashav Madan <kshvmdn@gmail.com>
2016-02-29 12:13:04 -0500

    Rename render methods

--- output.py
+++ output.py
@@ -7,9 +7,9 @@ from tabulate import tabulate
 def output(data, format_, fn):
     render = None
     if format_ == 'json':
-        fn, render = fn.format('json'), _render_json
+        fn, render = fn.format('json'), render_json
     elif format_ == 'table':
-        fn, render = fn.format('txt'), _render_table
+        fn, render = fn.format('txt'), render_table
 
     if render is not None:
         f = open(os.path.expanduser('~/Desktop/{}'.format(fn)), 'w')
@@ -18,9 +18,9 @@ def output(data, format_, fn):
     return pprint(data)
 
 
-def _render_json(data, file_):
+def render_json(data, file_):
     return file_.write(json.dumps(data, indent=4))
 
 
-def _render_table(data, file_):
+def render_table(data, file_):
     return file_.write(tabulate(data, headers='keys', tablefmt='fancy_grid'))
