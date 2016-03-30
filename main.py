eeab4ba8e6689bb6d518b236b7b84d84f6335261
Kashav Madan <kshvmdn@gmail.com>
2016-03-30 00:08:55 -0400

    Cleanup argparse, main
    sys.exit(1) instead of return on error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import argparse
 import sys
 
@@ -10,17 +9,28 @@ from output import output
 BASE_URL = 'http://www.artsci.utoronto.ca/current/exams/{}'
 
 parser = argparse.ArgumentParser(description='UofT Exam Schedule scraper')
-parser.add_argument('-s', dest="semester", type=str, default='W16',
-                    help='semester / year to get schedule for \
-                          (format: SYY, default: W16)')
-parser.add_argument('-c', dest='courses', nargs='+', required=True,
+parser.add_argument('-s', '--semester',
+                    dest="semester",
+                    type=str,
+                    default='W16',
+                    help='semester (S) + year (YY) to get schedule for \
+                          (format: SYY) [default: W16]')
+parser.add_argument('-c', '--courses',
+                    dest='courses',
+                    nargs='+',
+                    required=True,
                     help='courses (separated by a space, use dash for \
                           lecture code) [eg. csc148-l0101 csc165]')
-parser.add_argument('-ln', dest='last_name', type=str, required=True,
+parser.add_argument('-n', '--name',
+                    dest='last_name',
+                    type=str,
+                    required=True,
                     help='last name')
-parser.add_argument('-f', dest='format', type=str, default=None,
+parser.add_argument('-f', '--format',
+                    dest='format',
+                    type=str, default=None,
                     choices=['table', 'json'],
-                    help='output format (one of <table|json>, default: raw)')
+                    help='output format (one of <table|json>) [default: None]')
 
 
 def main():
@@ -29,8 +39,8 @@ def main():
         args.semester[0], args.semester[1:], args.format
 
     if season.upper() not in ('W', 'F') or not year.isnumeric():
-        print('Expected valid semester (should be of form SYY, e.g. F15, W16)')
-        sys.exit(0)
+        print('Unexpected semester argument (run -h for details).')
+        sys.exit(1)
 
     date = ('apr' if season == 'W' else 'dec') + year
 
@@ -38,11 +48,13 @@ def main():
     if exam_data is not None:
         user_exams = search(exam_data, courses, ln)
         if user_exams is not None:
-            return output(
-                user_exams, format_, '{}-exams'.format(ln) + '.{}')
-        return print('Couldn\'t find exam information for given courses.')
-    return print('Couldn\'t find exam information for given semester.')
-
+            return output(user_exams, format_, '{}-exams'.format(ln) + '.{}')
+        else:
+            print('Couldn\'t find exam information for given courses.')
+            sys.exit(1)
+    else:
+        print('Couldn\'t find exam information for given semester.')
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
